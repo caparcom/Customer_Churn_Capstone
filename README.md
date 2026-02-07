6146f7a5efe56eb0b648a23a47b73cd4e44cba1a
caparcom <caleb_combs@yahoo.com>
2026-02-06 18:03:30 -0600

    prep for external PostGres connection.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This project is designed to answer the following question: Can we predict water pump failures early enough to justify preventative means over costly emergency repairs in order to reduce economic damage, water contamination, etc.
 
+
 ## Contributors
 
 - Caleb Combs
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,22 @@
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-import pandas as pd
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
 
 # load the .env file variables
 load_dotenv()
 
-
 def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
+
+    engine = None
+    try:
+        engine = create_engine(os.getenv('DATABASE_URL'))
+        # Test the connection
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print("Connection successful")
+    except SQLAlchemyError as e:
+        print("Failed to connect to the database")
+        print("Error:", e)
+    
     return engine
